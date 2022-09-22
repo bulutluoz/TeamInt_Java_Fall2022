6ce1ac9f54815cfd80b3adb5d7fb34eb707f8104
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-09-22 21:58:37 +0200

    46.ders sonu

---
+++ src/day46_maps/C05_Maps.java
@@ -0,0 +1,45 @@
+package day46_maps;
+
+import day44_maps.ReusableMethods;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C05_Maps {
+
+    public static void main(String[] args) {
+        Map<String,Integer> ornekMap=new HashMap<>();
+
+        ornekMap.put("a",3);
+        ornekMap.put("b",1);
+        ornekMap.put("c",2);
+        ornekMap.put("d",5);
+
+        System.out.println(ornekMap); // {a=3, b=1, c=2, d=5}
+
+        System.out.println(ornekMap.containsValue(5)); // true
+
+        Map<Integer,String> ogrenciMap= ReusableMethods.ogrenciMapOlustur();
+        System.out.println(ogrenciMap);
+        // {101=Ali-Can-10-H-MF, 102=Veli-Cem-11-M-Soz, 103=Ali-Cem-11-B-TM, 104=Ayca-Can-11-B-MF, 105=Ayse-Cem-10-M-Soz}
+
+        System.out.println(ogrenciMap.containsValue("Ali")); // false
+        // containsValue("IstenenDeger") method'u tam olarak value girilirse true doner
+        // ancak value icerisindeki bir degeri aratirsaniz false doner
+        System.out.println(ogrenciMap.containsValue("Veli-Cem-11-M-Soz")); // true
+
+        System.out.println(ornekMap.replace("d", 10)); // eski degerini döndürür
+        System.out.println(ornekMap); // {a=3, b=1, c=2, d=10}
+
+        System.out.println(ornekMap.replace("a", 5, 8)); // false
+        System.out.println(ornekMap.replace("b",1,2)); // true
+        System.out.println(ornekMap); // {a=3, b=2, c=2, d=10}
+
+        System.out.println(ornekMap.get("b")); // 2
+        System.out.println(ornekMap.get("z")); // null
+        System.out.println(ornekMap.getOrDefault("a", 6)); // 3
+        System.out.println(ornekMap.getOrDefault("m", 0)); // 0
+        System.out.println(ornekMap); // {a=3, b=2, c=2, d=10}
+
+    }
+}
---
+++ src/day46_maps/C06_TreeMap.java
@@ -0,0 +1,61 @@
+package day46_maps;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class C06_TreeMap {
+    public static void main(String[] args) {
+
+        TreeMap<String,Integer> ornekMap=new TreeMap<>();
+
+        ornekMap.put("a",3);
+        ornekMap.put("r",1);
+        ornekMap.put("k",2);
+        ornekMap.put("b",5);
+
+        System.out.println(ornekMap); // {a=3, b=5, k=2, r=1}
+
+        System.out.println(ornekMap.ceilingEntry("b")); // b=5
+        System.out.println(ornekMap.ceilingEntry("c")); // k=2
+        System.out.println(ornekMap.ceilingEntry("m")); // r=1
+        System.out.println(ornekMap.ceilingEntry("t")); // null
+
+        System.out.println(ornekMap.ceilingKey("b")); // b
+        System.out.println(ornekMap.ceilingKey("c")); // k
+
+        System.out.println(ornekMap.firstKey());// a
+        System.out.println(ornekMap.firstEntry()); // a=3
+
+        System.out.println(ornekMap.lastKey()); // r
+        System.out.println(ornekMap.lastEntry()); // r=1
+
+        System.out.println(ornekMap.pollFirstEntry()); // a=3 silip getirdi
+        System.out.println(ornekMap.pollLastEntry()); // r=1 silip getirdi
+        System.out.println(ornekMap); // {b=5, k=2}
+
+        System.out.println(ornekMap.descendingMap()); // {k=2, b=5}
+        System.out.println(ornekMap); // {b=5, k=2}
+
+        System.out.println(ornekMap.floorEntry("b")); // b=5
+        System.out.println(ornekMap.floorKey("d")); // b
+        System.out.println(ornekMap.floorKey("k")); // k
+        System.out.println(ornekMap);
+
+        ornekMap.put("y",3);
+        ornekMap.put("t",2);
+        System.out.println(ornekMap); // {b=5, k=2, t=2, y=3}
+
+        System.out.println(ornekMap.headMap("m")); // {b=5, k=2}
+        System.out.println(ornekMap.headMap("t")); // {b=5, k=2}
+        System.out.println(ornekMap.headMap("t",true)); // {b=5, k=2, t=2}
+
+        System.out.println(ornekMap.higherKey("k")); // t
+        System.out.println(ornekMap.higherEntry("t")); // y=3
+
+        System.out.println(ornekMap.lowerEntry("m")); // k=2
+        System.out.println(ornekMap.lowerKey("c")); // b
+
+
+    }
+}
